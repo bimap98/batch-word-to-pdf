580da22527619e593e2fb75542c137d1a01d5d41
Bima Putra Lamanda <48901540+bimap98@users.noreply.github.com>
2020-11-11 09:57:40 +0700

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,5 @@
+from docx2pdf import convert
+
+convert("TUGAS MATA KULIAH DATA SAINS DENGAN TEMA.docx","E:\\tempat_convert\\") #one file to folder convert
+convert("E:\\tempat_word\\","E:\\tempat_convert\\") #batch pdf to word convert
+
